0322e85ec6e32b308fb2a347c2b05fda4464b4aa
Allan Evans <oneblankpage27@gmail.com>
2023-07-07 14:45:43 -0600

    Add the find method and the includes? method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -36,24 +36,72 @@ RSpec.describe LinkedList do
             expect(list.to_string).to eq("doop deep boop")
         end
     end
+    describe "#Add nodes to beginning and inserting" do
+    #Adding multiple nodes
+        it "add to beginning and middle of linked list" do
+            list = LinkedList.new
+            list.append("doop")
+            expect(list.count).to eq(1)
+            expect(list.to_string).to eq("doop")
+            
+            list.append("deep")
+            expect(list.count).to eq(2)
+            expect(list.to_string).to eq("doop deep")
 
-    it "add to begining of linked list" do
-        list = LinkedList.new
-        list.append("doop")
-        expect(list.count).to eq(1)
-        expect(list.to_string).to eq("doop")
-        
-        list.append("deep")
-        expect(list.count).to eq(2)
-        expect(list.to_string).to eq("doop deep")
+            list.append("boop")
+            expect(list.count).to eq(3)
+            expect(list.to_string).to eq("doop deep boop")
 
-        list.append("boop")
-        expect(list.count).to eq(3)
-        expect(list.to_string).to eq("doop deep boop")
+            list.prepend("dop")
+            expect(list.count).to eq(4)
+            expect(list.to_string).to eq("dop doop deep boop")
+            
+            list.insert("woo", 1)
+            expect(list.count).to eq(5)
+            expect(list.to_string).to eq("dop woo doop deep boop")
 
-        list.prepend("dop")
-        expect(list.count).to eq(4)
-        expect(list.to_string).to eq("dop doop deep boop")
-        
+            list.insert("woop", 6)
+            expect(list.count).to eq(6)
+            expect(list.to_string).to eq("dop woo doop deep boop woop")
+
+        end
+    end
+
+    describe "#Checkiing nodes" do
+    #Adding multiple nodes
+        it "find, remove, includes? testing" do
+            list = LinkedList.new
+            list.append("doop")
+            expect(list.count).to eq(1)
+            expect(list.to_string).to eq("doop")
+            
+            list.append("deep")
+            expect(list.count).to eq(2)
+            expect(list.to_string).to eq("doop deep")
+
+            list.append("boop")
+            expect(list.count).to eq(3)
+            expect(list.to_string).to eq("doop deep boop")
+
+            list.prepend("dop")
+            expect(list.count).to eq(4)
+            expect(list.to_string).to eq("dop doop deep boop")
+            
+            list.insert("woo", 1)
+            expect(list.count).to eq(5)
+            expect(list.to_string).to eq("dop woo doop deep boop")
+
+            list.insert("woop", 6)
+            expect(list.count).to eq(6)
+            expect(list.to_string).to eq("dop woo doop deep boop woop")
+
+            expect(list.find(2, 1)).to eq("doop")
+            expect(list.find(1, 3)).to eq("woo doop deep")
+            expect(list.find(0, 3)).to eq("dop woo doop")
+
+            expect(list.includes?("deep")).to eq(true)
+            expect(list.includes?("dep")).to eq(false)
+
+        end
     end
 end
